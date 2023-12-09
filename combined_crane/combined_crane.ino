55baac856fa4363c29041f28ac4de248462bcecf
ksmin1 <songheemin@berkeley.edu>
2023-12-09 11:50:49 -0800

    combined_crane complete

--- combined_crane/combined_crane.ino
+++ combined_crane/combined_crane.ino
@@ -1,18 +1,41 @@
+//Motors, Ultrasonic Sensor, 
 // motor 1 is bottom
-#define stepPin1 15
-#define dirPin1 32
+#define stepPin1 27 //15
+#define dirPin1 33 //32
 // motor 2 is top
-#define stepPin2 27
-#define dirPin2 33
+#define stepPin2 15 //27
+#define dirPin2 32 //33
 // Ultrasonic Sensor
 #define trigPin 17
 #define echoPin 21
 // // Electromagnet
 #define emPin 23
 
+#include "BluetoothSerial.h"
+String device_name = "Crane2";
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+  #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+// Check Serial Port Profile
+#if !defined(CONFIG_BT_SPP_ENABLED)
+  #error Serial Port Profile for Bluetooth is not available or not enabled. It is only available for the ESP32 chip.
+#endif
+
+BluetoothSerial SerialBT;
+char command = ' ';
+
+
 float duration, distance;
 
 void setup() {
+  Serial.begin(115200);
+
+  SerialBT.begin(device_name); //Bluetooth device name
+  //SerialBT.deleteAllBondedDevices(); // Uncomment this to delete paired devices; Must be called after begin
+  Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
+
   pinMode(stepPin1, OUTPUT);
   pinMode(dirPin1, OUTPUT);
   pinMode(stepPin2, OUTPUT);
@@ -20,7 +43,6 @@ void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(emPin, OUTPUT);
-  Serial.begin(115200);
 }
 
 void loop() {
@@ -34,79 +56,103 @@ void loop() {
   distance = (duration*.0343)/2;
   // Serial.print("Distance: ");
   // Serial.println(distance);
-  delay(100);
-
-  if (distance <30) {
-    Serial.println("Move M1");
-    digitalWrite(dirPin1,HIGH);
-    for (int x = 0; x<50; x++) {
-      digitalWrite(stepPin1,HIGH);
-      delayMicroseconds(20000);
-      digitalWrite(stepPin1, LOW);
-      delayMicroseconds(20000);
+  // delay(100);
+  if(SerialBT.available()) {
+    command = getBTCommand();
+  }
+  if(command == 'D' & distance < 30) {
+    dropPayload();
+    command = ' ';
+    while(command == ' ') {
+      command = SerialBT.read();
     }
-    delay(2000); // 2 second delay
+  }
+  else if(getBTCommand() == 'P') {
 
-    Serial.println("Lower M2");
-    digitalWrite(dirPin2,LOW); //lower
-    // turn on EM
-    digitalWrite(emPin, HIGH);
-    Serial.println("EM on");
-    for (int x = 0; x<50; x++) {
-      digitalWrite(stepPin2, HIGH);
-      delayMicroseconds(20000);
-      digitalWrite(stepPin2, LOW);
-      delayMicroseconds(20000);
-    }
-    delay(2000);
-
-    Serial.println("Raise M2");
-    digitalWrite(dirPin2, HIGH); // raise
-    for (int x = 0; x<50; x++) {
-      digitalWrite(stepPin2, HIGH);
-      delayMicroseconds(20000);
-      digitalWrite(stepPin2, LOW);
-      delayMicroseconds(20000);
-    }
-    delay(2000); // 2 second delay
-
-    Serial.println("Bring back M1");
-    digitalWrite(dirPin1, LOW); // change direction
-    for (int x = 0; x<50; x++) {
-      digitalWrite(stepPin1,HIGH);
-      delayMicroseconds(20000);
-      digitalWrite(stepPin1, LOW);
-      delayMicroseconds(20000);
-    }
-    delay(1000);
-
-    // lower M2
-    Serial.println("Lower M2");
-    digitalWrite(dirPin2,LOW); //lower
-    
-    for (int x = 0; x<50; x++) {
-      digitalWrite(stepPin2, HIGH);
-      delayMicroseconds(20000);
-      digitalWrite(stepPin2, LOW);
-      delayMicroseconds(20000);
-    }
-    digitalWrite(emPin, LOW);
-    // turn off EM
-    Serial.println("EM off");
-    delay(2000);
-
-    // raise M2
-    Serial.println("Raise M2");
-    digitalWrite(dirPin2, HIGH); // raise
-    for (int x = 0; x<50; x++) {
-      digitalWrite(stepPin2, HIGH);
-      delayMicroseconds(20000);
-      digitalWrite(stepPin2, LOW);
-      delayMicroseconds(20000);
-    }
-    delay(1000); // 2 second delay
   }
   else {
-    Serial.println("off");
+    serial.println("crane2 off")
   }
+}
+
+
+char getBTCommand() {
+  if (SerialBT.available()) {
+    //Serial.print("Reading Bluetooth");
+    char BTinput = SerialBT.read();
+    //Serial.print(BTinput);
+    return BTinput;
+  }
+}
+
+
+void dropPayload() {
+  Serial.println("Move M1");
+  digitalWrite(dirPin1,HIGH);
+  for (int x = 0; x<50; x++) {
+    digitalWrite(stepPin1,HIGH);
+    delayMicroseconds(20000);
+    digitalWrite(stepPin1, LOW);
+    delayMicroseconds(20000);
+  }
+  delay(2000); // 2 second delay
+
+  Serial.println("Lower M2");
+  digitalWrite(dirPin2,HIGH); //lower
+  // turn on EM
+  digitalWrite(emPin, HIGH);
+  Serial.println("EM on");
+  for (int x = 0; x<50; x++) {
+    digitalWrite(stepPin2, HIGH);
+    delayMicroseconds(20000);
+    digitalWrite(stepPin2, LOW);
+    delayMicroseconds(20000);
+  }
+  delay(2000);
+
+  Serial.println("Raise M2");
+  digitalWrite(dirPin2, LOW); // raise
+  for (int x = 0; x<50; x++) {
+    digitalWrite(stepPin2, HIGH);
+    delayMicroseconds(20000);
+    digitalWrite(stepPin2, LOW);
+    delayMicroseconds(20000);
+  }
+  delay(2000); // 2 second delay
+
+  Serial.println("Bring back M1");
+  digitalWrite(dirPin1, LOW); // change direction
+  for (int x = 0; x<50; x++) {
+    digitalWrite(stepPin1,HIGH);
+    delayMicroseconds(20000);
+    digitalWrite(stepPin1, LOW);
+    delayMicroseconds(20000);
+  }
+  delay(1000);
+
+  // lower M2
+  Serial.println("Lower M2");
+  digitalWrite(dirPin2,HIGH); //lower
+  
+  for (int x = 0; x<50; x++) {
+    digitalWrite(stepPin2, HIGH);
+    delayMicroseconds(20000);
+    digitalWrite(stepPin2, LOW);
+    delayMicroseconds(20000);
+  }
+  digitalWrite(emPin, LOW);
+  // turn off EM
+  Serial.println("EM off");
+  delay(2000);
+
+  // raise M2
+  Serial.println("Raise M2");
+  digitalWrite(dirPin2, LOW); // raise
+  for (int x = 0; x<50; x++) {
+    digitalWrite(stepPin2, HIGH);
+    delayMicroseconds(20000);
+    digitalWrite(stepPin2, LOW);
+    delayMicroseconds(20000);
+  }
+  delay(1000); // 2 second delay
 }
